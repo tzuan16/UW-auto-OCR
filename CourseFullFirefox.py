107345ecbe49d2cbca9545e76aaff9de090ab0fa
tzuan16 <hungan@uw.edu>
2020-09-14 17:46:18 +0800

    Add drop function

--- CourseFullFirefox.py
+++ CourseFullFirefox.py
@@ -24,47 +24,54 @@ def setUp(browser, username, password, sln, quarter, year):
     submit_button.click()
 
 
-def register(browser, sln, quiz):
+def dropCourses(browser, dropSlns):
+    for dropSln in dropSlns:
+        to_drop_checkbox = browser.find_element_by_xpath(
+            f'//tr[input/@value={dropSln}]/td/input')
+        to_drop_checkbox.click()
+
+
+def register(browser, lectureSln, quizSln, dropSlns):
     url = 'https://sdb.admin.uw.edu/students/uwnetid/register.asp'
     browser.get(url)
+    dropCourses(browser, dropSlns)
     sln_table_input = browser.find_element_by_xpath(
         '/html/body/div[2]/form[1]/p[2]/table[1]/tbody[1]/tr[2]/td[1]/input[1]')
-    sln_table_input.send_keys(sln)
-    if quiz != 'NA':
+    sln_table_input.send_keys(lectureSln)
+    if quizSln != 'NA':
         sln_table_input = browser.find_element_by_xpath(
             '/html/body/div[2]/form[1]/p[2]/table[1]/tbody[1]/tr[3]/td[1]/input[1]')
-        sln_table_input.send_keys(quiz)
+        sln_table_input.send_keys(quizSln)
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     submit_button = browser.find_element_by_xpath(
         '/html/body/div[2]/form[1]/input[7]')
     submit_button.click()
 
 
-def slnToSearchFor(sln, quiz):
-    if quiz != 'NA':
-        return quiz
-    return sln
+def slnToSearchFor(lectureSln, quizSln):
+    return quizSln if quizSln != 'NA' else lectureSln
 
 
 print('DEICLAIMER: This program can only help you register for the classes that you are allowed to enroll in during the period')
 print('Use Ctrl + C to terminate')
 print()
-print('Your UW NetID?')
-username = input()
-print('Your UW NetID password?')
-password = input()
-print('SLN of the course you want? (not the quiz section)')
-sln = input()
-print('SLN of the quiz section you want? (enter NA of there is no quiz section)')
-quiz = input()
-print('Which quarter is this for? choose 1 from (AUT, WIN, SPR, SUM)')
-quarter = input()
-print('What year is this for? (e.g. 2020)')
-year = input()
+
+username = input('Your UW NetID?\n')
+password = input('Your UW NetID password?\n')
+quarter = input(
+    'Which quarter is this for? choose 1 from (AUT, WIN, SPR, SUM)\n')
+year = input('What year is this for? (e.g. 2020)\n')
+lectureSln = input('SLN of the lecture you want?\n')
+quizSln = input(
+    'SLN of the quiz section you want? (enter NA if there is no quiz section)\n')
+dropSln = input(
+    'SLNs to drop? (enter NA if there is none)\ne.g. 10001 10002\n')
+dropSlns = dropSln.split()
+
 browser = webdriver.Firefox()
-finalSln = slnToSearchFor(sln, quiz)
+finalSln = slnToSearchFor(lectureSln, quizSln)
 setUp(browser, username, password, finalSln, quarter, year)
 while not check_availibility(browser):
     print('There are no empty seats now :(')
     browser.refresh()
-register(browser, sln, quiz)
+register(browser, lectureSln, quizSln, dropSlns)
